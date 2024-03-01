88a80968d85b94e54718fdbd9d9dca1c9c491ce0
vivit_jc <kazyu.vanilla@gmail.com>
2024-03-01 20:13:20 +0900

    first commit

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <q-btn @click="clickBtn(tree)">{{ tree.name }}:{{ tree.count }}</q-btn>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const tree = ref({ name: '木', count: 0 });
+
+function clickBtn(ref: any) {
+  ref.count += 1;
+  return true;
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <main-component></main-component>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import MainComponent from 'components/MainComponent.vue';
+</script>
