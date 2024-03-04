597648bd29faa9d7d370b5265f85b05f04b57a08
vivit_jc <kazyu.vanilla@gmail.com>
2024-03-04 22:11:09 +0900

    市民の要求の表示を追加

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,38 +1,56 @@
 <template>
-  <div>
-    資源
-    <q-btn
-      v-for="resource in resources"
-      :key="resource.name"
-      @click="clickBtn(resource)"
-    >
-      {{ resource.name }}:{{ resource.count }}
-    </q-btn>
-    <div>
-      　建築
-      <q-btn @click="clickbuildBtn(buildings, resources)"
-        >{{ buildings.name }}を建てる　</q-btn
-      >
+  <div class="row">
+    <div class="col-3">
+      資源<br />
+      <ul>
+        <li v-for="resource in resources" :key="resource.name">
+          <q-btn @click="clickBtn(resource)">
+            {{ resource.name }}:{{ resource.count }}
+          </q-btn>
+        </li>
+      </ul>
+      <div>
+        　建築<br />
+        <q-btn @click="clickbuildBtn(buildings, resources)"
+          >{{ buildings.name }}を建てる　</q-btn
+        >
+      </div>
+      　
+      <div v-for="building in buildings" :key="building.name">
+        建物 {{ building.name }}:{{ building.count }}
+      </div>
     </div>
-    　
-    <div v-for="building in buildings" :key="building.name">
-      建物 {{ building.name }}:{{ building.count }}
+    <div class="col-6">
+      <q-input v-model="name" label="名前" />
+      <q-input v-model="text" label="内容" />
+      <q-btn label="送信" color="primary" @click="submit()" />
+      <table border="1">
+        <tr>
+          <th>名前</th>
+          <th>内容</th>
+          <th>時刻</th>
+        </tr>
+        <tr v-for="p in posts" :key="p.timestamp">
+          <td>{{ p.name }}</td>
+          <td>{{ p.text }}</td>
+          <td>{{ strDate(p.timestamp) }}</td>
+        </tr>
+      </table>
     </div>
-    <q-input v-model="name" label="名前" />
-    <q-input v-model="text" label="内容" />
-    <q-btn label="送信" color="primary" @click="submit()" />
-    <table border="1">
-      <tr>
-        <th>名前</th>
-        <th>内容</th>
-        <th>時刻</th>
-      </tr>
-      <tr v-for="p in posts" :key="p.timestamp">
-        <td>{{ p.name }}</td>
-        <td>{{ p.text }}</td>
-        <td>{{ strDate(p.timestamp) }}</td>
-      </tr>
-    </table>
+    <div class="col-3">
+      要求<br />
+      <ul>
+        <li v-for="(demand, index) in demands" :key="index" class="people">
+          <div>
+            {{ demand.type }}<br />
+            <span v-for="d in demand.demand" :key="d.name">
+              {{ d.name }}:{{ d.count }}<br />
+            </span>
+          </div>
+          <q-btn color="black">要求を満たす</q-btn>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -59,6 +77,11 @@ interface Post {
   timestamp: number;
 }
 
+interface Demand {
+  type: string;
+  demand: Resource[];
+}
+
 const resources = ref<Resource[]>([
   { name: '木', count: 101 },
   { name: '鉄', count: 10 },
@@ -66,6 +89,19 @@ const resources = ref<Resource[]>([
   { name: '羊毛', count: 0 },
 ]);
 
+const demand_data: Demand[] = [
+  {
+    type: '農民',
+    demand: [
+      { name: '羊毛', count: rand(3) + 1 },
+      { name: '麦', count: rand(3) + 1 },
+    ],
+  },
+  { type: '農民', demand: [{ name: '鉄', count: rand(3) + 1 }] },
+  { type: '労働者', demand: [{ name: '木', count: rand(3) + 1 }] },
+];
+
+let demands = ref<Demand[]>(demand_data);
 const buildings = ref<Resource[]>([{ name: '家', count: 0 }]);
 
 watch(route, (n, p) => {
@@ -113,4 +149,18 @@ function strDate(ts: number) {
     date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate()
   );
 }
+
+function rand(n: number) {
+  return Math.floor(Math.random() * n);
+}
 </script>
+
+<style scoped>
+li {
+  list-style: none;
+}
+.people {
+  background-color: aqua;
+  margin-bottom: 5px;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="row items-center justify-evenly">
+  <q-page class="">
     <main-component></main-component>
   </q-page>
 </template>
